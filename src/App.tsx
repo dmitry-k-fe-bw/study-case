d58395b5e2fafc2c578beb68826b5fc5d139d67b
Nevo Chen <nevo.ch@bluewhite.co>
2023-10-24 10:34:29 +0300

    made a responsive sidebar. collapse and expand functionalities are working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,55 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
+    // <div className='page'>
+    //   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
+    //   <div className='sidebar expand'>
+    //     <div className='sidebar-content'>
+    //       <div className='sidebar-header'>
+    //         <div className='sidebar-logo'>
+    //           <i className="fa-regular fa-hospital"></i>
+    //           <p>VET.</p>
+    //         </div>
+    //         <i className="fa-solid fa-bars"></i>
+    //       </div>
+    //       <div className='search-box'>
+    //         <i className="fa-solid fa-magnifying-glass"></i>
+    //         <p>search</p>
+    //       </div>
+    //       <div className='sidebar-item'>
+    //       <i className="fa-solid fa-table-cells-large"></i>
+    //         <p>Dashboard</p>
+    //       </div>
+    //       <div className='sidebar-item'>
+    //       <i className="fa-solid fa-paw"></i>
+    //         <p>Pets</p>
+    //       </div>
+    //       <div className='sidebar-item'>
+    //       <i className="fa-regular fa-user"></i>
+    //         <p>Client</p>
+    //       </div>
+    //       <div className='sidebar-item'>
+    //       <i className="fa-solid fa-user-doctor"></i>
+    //         <p>Vets</p>
+    //       </div>
+    //       <div className='sidebar-item'>
+    //       <i className="fa-solid fa-gear"></i>
+    //         <p>Adjust</p>
+    //       </div>
+    //     </div>
+    //     <div className='sidebar-user'>
+    //       <p>Eleanor Pena</p>
+    //       <i className="fa-solid fa-arrow-right-from-bracket"></i>
+    //     </div>
+    //   </div>
+    //   <div className='content'>
+    //     <h2>Dashboard</h2>
+    //   </div>
+    // </div>
   );
 }
 
