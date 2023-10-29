24b164182d20d78bab31e0c8061005d9ee93c8f8
Nevo Chen <nevo.ch@bluewhite.co>
2023-10-29 13:53:57 +0200

    fixed the issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,8 @@
 import './App.css';
 
 function App() {
-
   return (
-    <>
-    </>
-    // <div className='page'>
-    //   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
-    //   <div className='sidebar expand'>
-    //     <div className='sidebar-content'>
-    //       <div className='sidebar-header'>
-    //         <div className='sidebar-logo'>
-    //           <i className="fa-regular fa-hospital"></i>
-    //           <p>VET.</p>
-    //         </div>
-    //         <i className="fa-solid fa-bars"></i>
-    //       </div>
-    //       <div className='search-box'>
-    //         <i className="fa-solid fa-magnifying-glass"></i>
-    //         <p>search</p>
-    //       </div>
-    //       <div className='sidebar-item'>
-    //       <i className="fa-solid fa-table-cells-large"></i>
-    //         <p>Dashboard</p>
-    //       </div>
-    //       <div className='sidebar-item'>
-    //       <i className="fa-solid fa-paw"></i>
-    //         <p>Pets</p>
-    //       </div>
-    //       <div className='sidebar-item'>
-    //       <i className="fa-regular fa-user"></i>
-    //         <p>Client</p>
-    //       </div>
-    //       <div className='sidebar-item'>
-    //       <i className="fa-solid fa-user-doctor"></i>
-    //         <p>Vets</p>
-    //       </div>
-    //       <div className='sidebar-item'>
-    //       <i className="fa-solid fa-gear"></i>
-    //         <p>Adjust</p>
-    //       </div>
-    //     </div>
-    //     <div className='sidebar-user'>
-    //       <p>Eleanor Pena</p>
-    //       <i className="fa-solid fa-arrow-right-from-bracket"></i>
-    //     </div>
-    //   </div>
-    //   <div className='content'>
-    //     <h2>Dashboard</h2>
-    //   </div>
-    // </div>
+    <></>
   );
 }
 
--- src/html-script.js
+++ src/html-script.js
@@ -1,4 +1,4 @@
 document.getElementById('toggle-sidebar').addEventListener('click',function () {
-    const sidebar = document.getElementById('sidebar');
+    const sidebar = document.querySelector('aside');
     sidebar.classList.toggle('collapsed');
 });
